e7a3afea4c9c277a5de04d9d844874379505cf91
“Vivek <vivekbadiani@gmail.com>
2023-03-14 10:33:15 -0600

    updated layour background colour

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -60,7 +60,7 @@ function App() {
       // Save user to MongoDB here...
 
       if (profileObj) {
-        const response = await fetch("http://localhost:8080/api/v1/users", {
+        const response = await fetch("https://property-dashboard-ioic.onrender.com/api/v1/users", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -129,7 +129,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://property-dashboard-ioic.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
