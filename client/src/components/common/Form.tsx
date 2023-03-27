4e345ea6aa88673537395c838e14ca3951535803
“Vivek <vivekbadiani@gmail.com>
2023-03-27 08:47:50 -0600

    updated TextField styling

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -54,15 +54,20 @@ const Form = ({
                 Experiment Title
               </FormHelperText>
               <TextField
-                style={{
-                  width: "100%",
-                  background: "transparent",
-                  fontSize: "16px",
-                  borderColor: "rgba(0,0,0,0.23)",
-                  borderRadius: 6,
-                  padding: 10,
-                  color: "#919191",
+                sx={{
+                  input: {
+                    color: "black",
+                  },
                 }}
+                // style={{
+                //   width: "100%",
+                //   background: "transparent",
+                //   fontSize: "16px",
+                //   borderColor: "rgba(0,0,0,0.23)",
+                //   borderRadius: 6,
+                //   padding: 10,
+                //   color: "#919191",
+                // }}
                 width="75%"
                 required
                 id="outlined-basic"
