ba8749f4d1247137a761498c4af8e8fd973a39fc
“Vivek <vivekbadiani@gmail.com>
2023-05-31 15:08:28 -0600

    localhost updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -69,7 +69,7 @@ function App() {
 
       if (profileObj) {
         const response = await fetch(
-          "https://catgen.onrender.com/api/v1/users", //https://catgen.onrender.com/api/v1/users //http://localhost:8080/api/v1/users
+          "http://localhost:8080/api/v1/users", //https://catgen.onrender.com/api/v1/users //http://localhost:8080/api/v1/users
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -140,7 +140,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("https://catgen.onrender.com/api/v1")} //http://localhost:8080/api/v1 //https://catgen.onrender.com/api/v1
+          dataProvider={dataProvider("http://localhost:8080/api/v1")} //http://localhost:8080/api/v1 //https://catgen.onrender.com/api/v1
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
